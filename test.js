97a139a1481bfa97c6e24aa38a9818d1d69107b1
E.Azer Koçulu <azer@kodfabrik.com>
2014-03-23 02:48:58 -0700

    test other delimiters

--- test.js
+++ test.js
@@ -1,50 +1,65 @@
+var test = require('prova');
 var routeMap = require("./");
-var match;
 
-before(function(done){
-  match = routeMap({
-    '/people/:name': user,
-    '/fruits/:fruit': fruit,
-    '/fruits/:fruit/:page': fruitPage,
-    '/': home
+var match = routeMap({
+  '/people/:name': user,
+  '/fruits/:fruit': fruit,
+  '/fruits/:fruit/:page': fruitPage,
+  '/:with.:dots,:and;:commas': dots,
+  '/': home
+});
+
+test('returns an object with matching function', function (t) {
+  t.equal(match('http://foobar.com/people/azer').fn, user);
+  t.equal(match('http://localhost/fruits/orange').fn, fruit);
+  t.equal(match('http://localhost:8080/fruits/orange/1/').fn, fruitPage);
+  t.equal(match('http://localhost/').fn, home);
+  t.end();
+});
+
+test('should return undefined if url doesnt match anything on the table', function (t) {
+  t.notOk(match('http://foobar.com/fruits/orange/1/2'));
+  t.notOk(match('http://localhost/fruits'));
+  t.notOk(match('http://localhost:8080/f'));
+  t.end();
+});
+
+test('should return an object named param with the matching url parts', function (t) {
+  t.equal(match('http://foobar.com/people/azer').params.name, 'azer');
+  t.equal(match('http://localhost/fruits/orange').params.fruit, 'orange');
+  t.equal(match('http://localhost:8080/fruits/orange/1/').params.fruit, 'orange');
+  t.equal(match('http://localhost:8080/fruits/orange/5/').params.page, '5');
+  t.end();
+});
+
+test('should return the matching pattern', function (t) {
+  t.equal(match('http://foobar.com/people/azer').pattern, '/people/:name');
+  t.end();
+});
+
+test('should return url', function (t) {
+  t.equal(match('http://foobar.com/people/azer?foo=123#bar=qux').url, 'http://foobar.com/people/azer?foo=123#bar=qux');
+  t.end();
+});
+
+test('should return parsed qs', function (t) {
+  t.equal(match('http://foobar.com/people/azer?foo=bar&qux=corge').qs.foo, 'bar');
+  t.equal(match('http://foobar.com/people/azer?foo=bar&qux=corge#qux=eggs').qs.qux, 'corge');
+  t.end();
+});
+
+test('with dots and commas', function (t) {
+  t.deepEqual(match('http://foo/lorem.ipsum,sit;dolor').params, {
+    'with': 'lorem',
+    'dots': 'ipsum',
+    'and': 'sit',
+    'commas': 'dolor'
   });
-  done();
-});
-
-it('returns an object with matching function', function(){
-  expect(match('http://foobar.com/people/azer').fn).to.equal(user);
-  expect(match('http://localhost/fruits/orange').fn).to.equal(fruit);
-  expect(match('http://localhost:8080/fruits/orange/1/').fn).to.equal(fruitPage);
-  expect(match('http://localhost/').fn).to.equal(home);
-});
-
-it('should return undefined if url doesnt match anything on the table', function(){
-  expect(match('http://foobar.com/fruits/orange/1/2')).to.not.exist;
-  expect(match('http://localhost/fruits')).to.not.exist
-  expect(match('http://localhost:8080/f')).to.not.exist
-});
-
-it('should return an object named param with the matching url parts', function(){
-  expect(match('http://foobar.com/people/azer').params.name).to.equal('azer');
-  expect(match('http://localhost/fruits/orange').params.fruit).to.equal('orange');
-  expect(match('http://localhost:8080/fruits/orange/1/').params.fruit).to.equal('orange');
-  expect(match('http://localhost:8080/fruits/orange/5/').params.page).to.equal('5');
-});
-
-it('should return the matching pattern', function(){
-  expect(match('http://foobar.com/people/azer').pattern).to.equal('/people/:name');
-});
-
-it('should return url', function(){
-  expect(match('http://foobar.com/people/azer?foo=123#bar=qux').url).to.equal('http://foobar.com/people/azer?foo=123#bar=qux');
-});
-
-it('should return parsed qs', function(){
-  expect(match('http://foobar.com/people/azer?foo=bar&qux=corge').qs.foo).to.equal('bar');
-  expect(match('http://foobar.com/people/azer?foo=bar&qux=corge#qux=eggs').qs.qux).to.equal('corge');
+  t.end();
 });
 
 function home () {}
 function user () {}
 function fruit () {}
 function fruitPage () {}
+function dots () {}
